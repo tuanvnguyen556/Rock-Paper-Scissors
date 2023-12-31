d5980bf5813426812b9819b427178e38f8f8afcb
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-12-31 13:15:50 -0800

    completed up to game

--- rps.js
+++ rps.js
@@ -1,34 +1,34 @@
 
 
 let imageObject = ["images/another-group-photo.jpg", "images/group-of-children.jpeg", "images/02.jpg", "images/group-photo.jpeg"];
-function goLeft(){
+function goLeft() {
     let val = JSON.parse(localStorage.getItem("val"));
     console.log(val);
-    if (val === null){
+    if (val === null) {
         val = 0;
-    }else{
+    } else {
         val = Number(val);
     }
-    if (val === 0){
+    if (val === 0) {
         val = imageObject.length - 1;
-    } else{
+    } else {
         val -= 1;
     }
     localStorage.setItem("val", JSON.stringify(val));
     document.querySelector(".display-image").setAttribute("src", imageObject[val]);
 }
 
-function goRight(){
+function goRight() {
     let val = JSON.parse(localStorage.getItem("val"));
-    console.log(val);
-    if (val === null){
+
+    if (val === null) {
         val = 0;
-    } else{
+    } else {
         val = Number(val);
     }
-    if (val === imageObject.length - 1){
+    if (val === imageObject.length - 1) {
         val = 0;
-    } else{
+    } else {
         val += 1;
     }
     localStorage.setItem("val", JSON.stringify(val));
@@ -36,11 +36,11 @@ function goRight(){
 }
 function buttonBars() {
     const listElement = document.querySelector(".list");
- 
-    if (listElement.style.display === "none" || listElement.style.display === ""){
+
+    if (listElement.style.display === "none" || listElement.style.display === "") {
         listElement.style.display = "flex";
     }
-    else{
+    else {
         listElement.style.display = "none";
     }
 }
@@ -52,84 +52,97 @@ draggables.forEach(draggable => {
     draggable.addEventListener("dragstart", () => {
         draggable.classList.add("dragged");
         let computerContain = document.querySelector(".computer-move");
-        let changeText = document.querySelector(".Results");
-        changeText.innerHTML = "";
-        computerContain.removeChild(computerContain.firstChild);
-      
+        let userContain = document.querySelector(".button-selection");
+
+        console.log(userContain.childElementCount);
+        let changeText = document.querySelector(".Result-phrase");
+
+        if (computerContain.childElementCount === 1) {
+            computerContain.removeChild(computerContain.firstChild);
+            changeText.innerText = "";
+        }
+
+
     })
 
-    draggable.addEventListener("dragend", ()=>{
+    draggable.addEventListener("dragend", () => {
         const itemContainer = document.querySelector(".made-move");
         const computerContainer = document.querySelector(".computer-move");
+        let storedMove = document.querySelector(".store-moves");
+        let changeText = document.querySelector(".Result-phrase");
+        const dragged = document.querySelector(".dragged");
 
-        if (itemContainer.childElementCount == 1){
+        if (itemContainer.childElementCount == 1) {
+
             let computerMove = Math.random();
             let newItalic = document.createElement("i");
             let newButton = document.createElement("button");
-            let changeText = document.querySelector(".Results");
+            let changeText = document.querySelector(".Result-phrase");
             /*grass is < 0.33*/
-            if (computerMove < 0.33){ 
+            if (computerMove < 0.33) {
                 newButton.className = "drag-buttons grass";
                 newItalic.className = "fa-solid fa-road-spikes";
                 newButton.appendChild(newItalic);
-                
-                if (draggable.className === "drag-buttons grass dragged"){
-                    changeText.innerHTML = "You tied!";
-                } else if(draggable.className === "drag-buttons fire dragged") {
-                    changeText.innerHTML = "You won!";
+
+                if (draggable.className === "drag-buttons grass dragged") {
+                    changeText.innerText = "You tied!";
+                } else if (draggable.className === "drag-buttons fire dragged") {
+                    changeText.innerText = "You won!";
                 }
-                else{
-                    changeText.innerHTML = "You lost!";
+                else {
+                    changeText.innerText = "You lost!";
                 }
                 console.log("computer is less than 0.3");
-            /*water is >= 0.33 and < 0.66*/
-            } else if (computerMove >= 0.33 && computerMove < 0.66){
+                /*water is >= 0.33 and < 0.66*/
+            } else if (computerMove >= 0.33 && computerMove < 0.66) {
                 newButton.className = "drag-buttons water";
                 newItalic.className = "fa-solid fa-hand-holding-droplet";
                 newButton.appendChild(newItalic);
 
-                if (draggable.className === "drag-buttons grass dragged"){
-                    changeText.innerHTML = "You won!";
-                } else if(draggable.className === "drag-buttons fire dragged") {
-                    changeText.innerHTML = "You lost!";
+                if (draggable.className === "drag-buttons grass dragged") {
+                    changeText.innerText = "You won!";
+                } else if (draggable.className === "drag-buttons fire dragged") {
+                    changeText.innerText = "You lost!";
                 }
-                else{
-                    changeText.innerHTML = "You tied!";
+                else {
+                    changeText.innerText = "You tied!";
                 }
                 console.log("computer is in between");
-            /*fire is else*/
-            } else{
+                /*fire is else*/
+            } else {
                 newButton.className = "drag-buttons fire";
                 newItalic.className = "fa-solid fa-fire";
                 newButton.appendChild(newItalic);
-                
-                if (draggable.className === "drag-buttons grass dragged"){
-                    
-                    changeText.innerHTML = "You lossed!";
-                } else if(draggable.className === "drag-buttons fire dragged") {
-                    changeText.innerHTML = "You tied!";
+
+                if (draggable.className === "drag-buttons grass dragged") {
+
+                    changeText.innerText = "You lost!";
+                } else if (draggable.className === "drag-buttons fire dragged") {
+                    changeText.innerText = "You tied!";
                 }
-                else{
-                    changeText.innerHTML = "You won!";
+                else {
+                    changeText.innerText = "You won!";
                 }
                 console.log("computer is last");
             }
             computerContainer.appendChild(newButton);
+        } else if (storedMove.childElementCount == 3){
+            changeText.innerText = "The Results are...";
         }
         draggable.classList.remove("dragged");
     })
 })
 
 containers.forEach(container => {
-    container.addEventListener("dragover", e=> {
+    container.addEventListener("dragover", e => {
         e.preventDefault();
         const draggable = document.querySelector(".dragged");
         const itemContainer = document.querySelector(".made-move");
 
-        if (itemContainer.childElementCount === 0){
+        if (itemContainer.childElementCount === 0) {
             container.appendChild(draggable);
         }
-        if (container != itemContainer){
+        if (container != itemContainer) {
             container.appendChild(draggable);
         }
     })
